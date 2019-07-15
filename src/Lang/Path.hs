c68255738f25b485a0d2783b195601b17c34fba3
Devin Lehmacher <lehmacdj@gmail.com>
2019-07-15 17:31:06 -0400

    add description to path

--- src/Lang/Path.hs
+++ src/Lang/Path.hs
@@ -1,10 +1,15 @@
+{-|
+   Describe sets of nodes using path specifications.
+   Given a graph and a node, a path denotes a set of nodes relative to the
+   starting node.
+ -}
 module Lang.Path where
 
 data Path t
   = Zero
   | One
   | Literal t
-  | Path t :. Path t
-  | Path t :+ Path t
-  | Path t :& Path t
+  | Path t :. Path t -- ^ sequence
+  | Path t :+ Path t -- ^ union
+  | Path t :& Path t -- ^ intersection
   deriving (Show, Eq, Ord)
