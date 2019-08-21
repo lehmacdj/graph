e7bac2a592604b59f07d3285d8b8d470aa913e9d
Devin Lehmacher <lehmacdj@gmail.com>
2019-08-21 14:10:35 -0700

    add load

---
+++ src/Effect/Load.hs
@@ -0,0 +1,12 @@
+{-# LANAGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Effect.Load where
+
+import ClassyPrelude
+
+import Control.Monad.Freer.TH
+
+data Load r where
+  SetLoaded :: String -> Load ()
+makeEffect ''Load
