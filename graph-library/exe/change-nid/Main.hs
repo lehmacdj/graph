d7a117752a96fe301c44dbe6e6f8ab35398151a7
Devin Lehmacher <lehmacdj@gmail.com>
2020-10-10 22:48:33 -0700

    add executable for rewriting node ids in ad hoc ways

---
+++ graph-library/exe/change-nid/Main.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Main (main) where
+
+import Control.Arrow ((>>>))
+import Effect.Graph
+import Effect.Graph.Advanced
+import Effect.Throw
+import Effect.Warn
+import Graph.Connect
+import Graph.Node
+import Graph.Types
+import MyPrelude
+import UserError
+
+nodeRewrites :: [NID] -> [(NID, NID)]
+nodeRewrites nids =
+  (filter outOfRange nids)
+    `zip` ([(maximumEx (filter (not . outOfRange) nids) + 1) ..])
+  where
+    outOfRange x = x >= 4000 || x < 0
+
+applyRewrite ::
+  Members [ReadGraph String, WriteGraph String, ThrowUserError] effs =>
+  (NID, NID) ->
+  Eff effs ()
+applyRewrite (nid, nid') = subsumeMissing $ do
+  n <- getNode' nid
+  let i = selfLoopify nid nid' $ incomingConnectsOf n
+      o = selfLoopify nid nid' $ outgoingConnectsOf n
+  insertNode @String (Node nid' i o (dataOf n))
+  deleteNode @String nid
+
+renumberNodes ::
+  ( Members [ReadGraph String, WriteGraph String, ThrowUserError] effs,
+    LastMember IO effs
+  ) =>
+  Eff effs ()
+renumberNodes = do
+  nodes <- nodeManifest @String
+  traverse_ applyRewrite $ nodeRewrites nodes
+
+runReadWriteGraphIO ::
+  FilePath ->
+  Eff
+    [ WriteGraph String,
+      ReadGraph String,
+      Warn UserErrors,
+      ThrowUserError,
+      IO
+    ]
+    () ->
+  IO ()
+runReadWriteGraphIO dir =
+  runWriteGraphIO dir
+    >>> runReadGraphIO dir
+    >>> printWarnings @UserErrors
+    >>> printErrors
+    >>> runM
+
+main :: IO ()
+main = do
+  args :: [String] <- map unpack <$> getArgs
+  case index args 0 of
+    Nothing -> putStrLn . pack $ "needs one command line argument"
+    Just dir -> runReadWriteGraphIO dir $ renumberNodes
