40b7a9be98ba7b530d55e3794ff30a5fc589001b
Devin Lehmacher <lehmacdj@gmail.com>
2020-11-08 23:57:45 -0800

    add test case for ParseSpec

---
+++ core/test/Lang/Command/ParseSpec.hs
@@ -0,0 +1,29 @@
+module Lang.Command.ParseSpec where
+
+import Data.Either (isLeft)
+import Lang.APath
+import Lang.Command
+import Lang.Command.Parse
+import Lang.Path
+import TestPrelude
+import Text.Megaparsec
+
+test_parseCommand :: TestTree
+test_parseCommand =
+  testGroup
+    "parseCommand"
+    [ "t hello/world #"
+        `parsesTo` Tag (Relative (Literal "hello" :/ Literal "world")) (Relative One),
+      "tag \"hello/world\" #"
+        `parsesTo` Tag (Relative (Literal "hello/world")) (Relative One),
+      -- regression test: this used to parse to t ouch hello-world; incorrectly
+      -- not requiring a space between t and ouch
+      parseFails "touch hello-world"
+    ]
+  where
+    parsesTo string expected =
+      testCase ("parse: " ++ show string) $
+        Right expected @=? parseCommand string
+    parseFails string =
+      testCase ("doesn't parse: " ++ show string) $
+        assert (isLeft (parseCommand string))
