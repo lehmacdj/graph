aed2b7f71a3ce1b29d48d500e79cf1216fff0e34
Devin Lehmacher <lehmacdj@gmail.com>
2020-11-09 00:13:26 -0800

    add TODO

---
+++ core/test/Lang/ParsingSpec.hs
@@ -0,0 +1,7 @@
+module Lang.ParsingSpec where
+
+import Lang.Parsing
+import TestPrelude
+
+-- TODO: add test case for commandFrom/command, see commit
+-- 77f5da460bd1d6b28fa29c27049f16dfbdd685b1 for bug that requires fix
