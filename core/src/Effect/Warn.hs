4ef4cc4903798a7318464b82deb496b3f96f7851
Devin Lehmacher <lehmacdj@gmail.com>
2020-11-10 23:33:13 -0800

    implement helpers for warn

--- core/src/Effect/Warn.hs
+++ core/src/Effect/Warn.hs
@@ -5,17 +5,21 @@ module Effect.Warn where
 
 import Effect.Throw
 import MyPrelude
+import UserError
 
 data Warn e m r where
   Warn :: e -> Warn e m ()
 
 makeSem ''Warn
 
--- warn :: Member (Warn e) effs => e -> Sem effs ()
--- warn e = send (Warn e)
-
 convertError :: forall e effs. Member (Warn e) effs => Sem (Error e : effs) () -> Sem effs ()
 convertError = (`handleError` warn)
+
+warnString :: Member (Warn UserErrors) r => String -> Sem r ()
+warnString = warnSingle . OtherError
+
+warnSingle :: Member (Warn UserErrors) r => UserError -> Sem r ()
+warnSingle = warn . singleton
 
 printWarnings ::
   forall e effs.
