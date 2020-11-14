6deba18be0122a51a2cf83ca57a949096915679e
Devin Lehmacher <lehmacdj@gmail.com>
2020-11-14 12:02:13 -0800

    implement interpreter for FreshNID effect

--- core/src/Effect/FreshNID.hs
+++ core/src/Effect/FreshNID.hs
@@ -4,12 +4,24 @@
 -- |
 -- Effect for generating unique identifiers. To be implemented by random
 -- generation of UUIDs or sequencial indicies for example.
-module Effect.FreshNID where
+module Effect.FreshNID
+  ( runFreshNIDState,
+    freshNID,
+    FreshNID (..),
+  )
+where
 
+import Effect.Util
 import Graph (NID)
 import MyPrelude
+import Polysemy.State
 
 data FreshNID m r where
   FreshNID :: FreshNID m NID
 
 makeSem ''FreshNID
+
+-- | Run FreshNID as a computation with a state representing the next value to
+-- use for a Fresh NID.
+runFreshNIDState :: Member (State NID) r => Sem (FreshNID : r) a -> Sem r a
+runFreshNIDState = interpret $ \FreshNID -> modifying @NID (+ 1)
