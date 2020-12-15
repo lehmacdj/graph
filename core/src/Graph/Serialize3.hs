9001b0d5a2b403d811e4c63c8792781d3a4ee269
Devin Lehmacher <lehmacdj@gmail.com>
2020-12-14 22:29:40 -0800

    make FilePath first arg uniformly

--- core/src/Graph/Serialize3.hs
+++ core/src/Graph/Serialize3.hs
@@ -10,6 +10,12 @@
 --   The auxilliary data is available from the console, and should be thought of
 --   as an unique edge to a file.
 --
+--   = Usage instructions
+--   All functions that act on the graph take a FilePath as the first argument.
+--   This must be a valid path to a directory otherwise a IOError may be thrown.
+--   Functions in this module may throw exceptions, improving exception safety
+--   is a goal.
+--
 --   = Differences from Graph.Serialize2
 --   For the most part this format is the same as Graph.Serialize2. The only
 --   differnces of note are:
@@ -71,8 +77,8 @@ nodeDataFile base nid = base </> (show nid ++ ".data")
 -- TODO: rewrite using System.Directory.Tree
 -- yields better error handling that isn't quite as sketchy
 
-serializeNodeEx :: MonadIO m => Node' -> FilePath -> m ()
-serializeNodeEx n base = liftIO $ do
+serializeNodeEx :: MonadIO m => FilePath -> Node' -> m ()
+serializeNodeEx base n = liftIO $ do
   createDirectoryIfMissing True base
   B.writeFile (linksFile base (nidOf n)) (Aeson.encode n)
   case dataOf n of
@@ -97,12 +103,12 @@ removeNode base nid = do
 
 -- | Execute a function on a node stored in the filesystem at a specified location
 -- ignore nodes that don't exist or if an error occurs
-withSerializedNode :: MonadIO m => (Node' -> Node') -> FilePath -> NID -> m ()
-withSerializedNode f base nid = liftIO $ do
+withSerializedNode :: MonadIO m => FilePath -> (Node' -> Node') -> NID -> m ()
+withSerializedNode base f nid = liftIO $ do
   maybeNode <- deserializeNode base nid
   case maybeNode of
     Left _ -> pure ()
-    Right n -> ignoreIOError $ serializeNodeEx (f n) base
+    Right n -> ignoreIOError $ serializeNodeEx base (f n)
 
 readGraph :: MonadIO m => FilePath -> m (Either String Graph')
 readGraph base = do
