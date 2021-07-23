c21f6b8d1fcad6b3b4073c8c33646a68977f4d5e
Devin Lehmacher <lehmacdj@gmail.com>
2021-07-22 23:02:35 -0700

    use Readline when applicable

--- core/src/Effect/Console.hs
+++ core/src/Effect/Console.hs
@@ -4,9 +4,12 @@
 module Effect.Console where
 
 import MyPrelude
+import Polysemy.Readline
 import System.IO.Term.Image
 
 -- | TODO: make this use Console.Readline for thread safe writing with prompt
+-- even for DisplayImage. This will require some thought about how to write
+-- raw bytestring to the console given only strings
 data Console m a where
   -- | print a string followed by a newline
   Echo :: String -> Console m ()
@@ -16,9 +19,9 @@ data Console m a where
 makeSem ''Console
 
 interpretConsoleIO ::
-  Member (Embed IO) effs =>
+  (Member Readline effs, Member (Embed IO) effs) =>
   Sem (Console : effs) a ->
   Sem effs a
 interpretConsoleIO = interpret $ \case
-  Echo s -> liftIO $ putStrLn (pack s)
+  Echo s -> outputStrLn (pack s)
   DisplayImage i -> liftIO $ printImage i
