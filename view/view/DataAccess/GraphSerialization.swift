fb76f017376f7d703d4d7a91c850e842336f85b7
Devin Lehmacher <lehmacdj@gmail.com>
2023-08-06 23:06:50 -0400

    separate tags out of GraphModels

--- view/view/DataAccess/GraphSerialization.swift
+++ view/view/DataAccess/GraphSerialization.swift
@@ -14,50 +14,6 @@ extension NID {
     static let tags: NID = 1
 }
 
-struct Tags<N: Node> {
-    let tagNode: N
-    let root: GraphManager<N>
-
-    var tagOptions: Set<String> {
-        return Set(tagNode.meta.outgoing.keys)
-    }
-
-    var tagNids: Set<NID> {
-        return Set(tagNode.meta.outgoing.values.flatMap { $0 })
-    }
-
-    func modify(for node: N, adding toAdd: Set<String>, removing toRemove: Set<String>) async {
-        logDebug("node: \(node.nid), adding: \(toAdd), removing: \(toRemove)")
-        assert(toAdd.intersection(toRemove).isEmpty, "can't add and remove the same tag")
-
-        for tag in toAdd {
-            let tagContainers = await tagNode[tag]
-            if tagContainers.isEmpty {
-                logWarn("can't create new node yet")
-                continue
-            } else if tagContainers.count > 1 {
-                logWarn("more than one node with same transition as tag, skipping: \(tag)")
-                continue
-            }
-            let tagContainer = tagContainers[0]
-            await root.addLink(from: tagContainer, to: node, via: "")
-        }
-
-        for tag in toRemove {
-            let tagContainers = await tagNode[tag]
-            if tagContainers.isEmpty {
-                logWarn("can't create new node yet")
-                continue
-            } else if tagContainers.count > 1 {
-                logWarn("more than one node with same transition as tag, skipping: \(tag)")
-                continue
-            }
-            let tagContainer = tagContainers[0]
-            await root.removeLink(from: tagContainer, to: node, via: "")
-        }
-    }
-}
-
 struct NodeTransition: Identifiable, Comparable, Hashable {
     let transition: String
     let nid: NID
