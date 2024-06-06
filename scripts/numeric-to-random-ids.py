746bb935efbe0486f9c1e4ca9f39cf8f8e56fa40
Devin Lehmacher <lehmacdj@gmail.com>
2024-06-05 23:57:37 -0400

    make more robust

--- scripts/numeric-to-random-ids.py
+++ scripts/numeric-to-random-ids.py
@@ -9,7 +9,7 @@ import sys
 # Predefined ID mappings
 predefined_id_map = {
     76: 'AhQufiPzgyRf',
-    # 1: 'pbYxBO6fzBQV',
+    1: 'pbYxBO6fzBQV',
     881: 'a0fVkm0kR7KE',
     9780: 'S00KkOYoVpFu',
     0: '000000000000'
@@ -30,23 +30,17 @@ def process_json_files(directory):
         if filename.endswith('.json'):
             file_path = os.path.join(directory, filename)
 
+            file_id = int(filename.split('.')[0])
             with open(file_path, 'r') as file:
                 data = json.load(file)
-
-            # Map the top-level ID
+            data_id = None
             if 'id' in data and isinstance(data['id'], int):
                 old_id = data['id']
-                if old_id not in id_map:
-                    id_map[old_id] = generate_random_id()
+            assert(data_id == file_id)
+            old_id = data_id
 
-            # Map IDs in incoming and outgoing lists
-            for link_type in ['incoming', 'outgoing']:
-                if link_type in data and isinstance(data[link_type], list):
-                    for item in data[link_type]:
-                        if 'n' in item and isinstance(item['n'], int):
-                            old_id = item['n']
-                            if old_id not in id_map:
-                                id_map[old_id] = generate_random_id()
+            if old_id not in id_map:
+                id_map[old_id] = generate_random_id()
 
     # Function to replace numeric IDs with new alphanumeric IDs using the ID map
     def replace_ids(data):
@@ -56,14 +50,18 @@ def process_json_files(directory):
                 if key == 'id' and value in id_map:
                     new_data[key] = id_map[value]
                 elif key in ['incoming', 'outgoing'] and isinstance(value, list):
-                    new_data[key] = [{'t': item['t'], 'n': id_map[item['n']]} if item['n'] in id_map else item for item in value]
+                    new_data[key] = [
+                            {'t': item['t'], 'n': id_map[item['n']]}
+                            if item['n']
+                            in id_map
+                            else Error(f"invalid id {item['n']}")
+                            for item in value
+                    ]
                 else:
-                    new_data[key] = replace_ids(value)
+                    raise ValueError(f"Invalid key: {key}")
             return new_data
-        elif isinstance(data, list):
-            return [replace_ids(item) for item in data]
         else:
-            return data
+            raise ValueError(f"Invalid data type: {type(data)}")
 
     # Update all JSON files with the new IDs
     for filename in os.listdir(directory):
@@ -77,7 +75,7 @@ def process_json_files(directory):
 
             # Save the updated JSON back to the file
             with open(file_path, 'w') as file:
-                json.dump(updated_data, file, indent=4)
+                json.dump(updated_data, file, separators=(',', ':'))
 
     # Rename the JSON and corresponding .data files with new IDs
     for old_id, new_id in id_map.items():
