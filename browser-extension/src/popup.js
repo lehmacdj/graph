14385c01bec221565453310ee74fc1506634271e
Devin Lehmacher <lehmacdj@gmail.com>
2024-06-16 00:42:20 -0400

    add scaffolded browser extension

---
+++ browser-extension/src/popup.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("myButton").addEventListener("click", function () {
+    alert("Button clicked!");
+  });
+});
---
+++ browser-extension/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    background: "./src/background.js",
+    content: "./src/content.js",
+    popup: "./src/popup.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
