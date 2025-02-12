a2a8392efc8f9423ac9de56b913f33c9d2361d63
Devin Lehmacher <lehmacdj@gmail.com>
2025-02-11 23:18:28 -0500

    implement GraphDataEditing

---
+++ cli/src/Graph/GraphDataEditing.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Graph.GraphDataEditing where
+
+import Models.Graph
+import Models.NID
+import Models.Node
+import MyPrelude
+import Polysemy.State
+
+data GraphDataEditing m a where
+  GetRawData :: NID -> GraphDataEditing m (Maybe ByteString)
+  WriteRawData :: NID -> ByteString -> GraphDataEditing m ()
+  DeleteRawData :: NID -> GraphDataEditing m ()
+
+makeSem ''GraphDataEditing
+
+runInMemoryGraphDataEditing ::
+  forall t r a.
+  ValidTransition t =>
+  (Member (State (Graph t (Maybe ByteString))) r) =>
+  Sem (GraphDataEditing : r) a ->
+  Sem r a
+runInMemoryGraphDataEditing = interpret \case
+  GetRawData nid ->
+    gets @(Graph t (Maybe ByteString)) $ view (at nid . _Just . #augmentation)
+  WriteRawData nid bs ->
+    modify @(Graph t (Maybe ByteString)) $ at nid . _Just . #augmentation ?~ bs
+  DeleteRawData nid ->
+    modify @(Graph t (Maybe ByteString)) $ at nid . _Just . #augmentation .~ Nothing
