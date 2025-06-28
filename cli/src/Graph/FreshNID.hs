79e3d9dd756cdf70703246b74ac1ed4d28120e06
Devin Lehmacher <lehmacdj@gmail.com>
2025-06-28 13:06:45 -0400

    comment out PickNID as I'm uncertain we'll need it

--- cli/src/Graph/FreshNID.hs
+++ cli/src/Graph/FreshNID.hs
@@ -6,7 +6,6 @@
 module Graph.FreshNID
   ( runFreshNIDRandom,
     freshNID,
-    pickNID,
     FreshNID (..),
   )
 where
@@ -18,18 +17,19 @@ import System.Random
 
 data FreshNID m r where
   FreshNID :: FreshNID m NID
-  -- | Pick a single NID from a list of NIDs in a case where we generated too
-  -- many and need to pick one to use arbitrarily.
-  --
-  -- Doing this instead of just generating a new NID marginally reduces the
-  -- chances of an NID collision, though argably this entire approach is just
-  -- premature optimization.
-  --
-  -- e.g. consider the use in `materializePathAsGraph`'s `:&` case
-  -- where we want to merge all new targets into a single new target
-  -- (because `:&` semantically should produce only nodes such that both paths
-  -- lead to the same node)
-  PickNID :: [NID] -> FreshNID m NID
+
+-- -- | Pick a single NID from a list of NIDs in a case where we generated too
+-- -- many and need to pick one to use arbitrarily.
+-- --
+-- -- Doing this instead of just generating a new NID marginally reduces the
+-- -- chances of an NID collision, though argably this entire approach is just
+-- -- premature optimization.
+-- --
+-- -- e.g. consider the use in `materializePathAsGraph`'s `:&` case
+-- -- where we want to merge all new targets into a single new target
+-- -- (because `:&` semantically should produce only nodes such that both paths
+-- -- lead to the same node)
+-- PickNID :: [NID] -> FreshNID m NID
 
 makeSem ''FreshNID
 
@@ -42,6 +42,7 @@ runFreshNIDRandom ::
   Sem r a
 runFreshNIDRandom = interpret \case
   FreshNID -> embedStateful uniform
-  PickNID nids -> do
-    i <- embedStateful $ uniformR (0, length nids - 1)
-    pure $ nids `indexEx` i
+
+-- PickNID nids -> do
+--   i <- embedStateful $ uniformR (0, length nids - 1)
+--   pure $ nids `indexEx` i
