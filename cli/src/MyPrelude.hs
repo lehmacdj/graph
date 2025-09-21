c6f71b314bb6a36be7865d256b15467fea909fbc
Devin Lehmacher <lehmacdj@gmail.com>
2025-09-21 19:19:23 -0400

    Add generic-data for Generically/Generically1

--- cli/src/MyPrelude.hs
+++ cli/src/MyPrelude.hs
@@ -19,10 +19,12 @@ import Control.DeepSeq as X
 import Data.Coerce as X (Coercible, coerce)
 import Data.Functor.Contravariant as X
 import Data.Kind as X (Type)
+import Data.Orphans ()
 import Data.Void as X
 import Debug.Trace
 import GHC.Generics as X (Generic, Rep)
 import GHC.Stack as X (HasCallStack)
+import Generic.Data as X (Generically (..), Generically1 (..))
 import Language.Haskell.TH.Syntax as X (Lift)
 import MyPrelude.Collections as X
 import MyPrelude.EarlyReturn as X
