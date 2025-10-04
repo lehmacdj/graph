a2050f48845d79ad731ff9aa42c3954b13e24e81
Devin Lehmacher <lehmacdj@gmail.com>
2025-10-04 14:08:04 -0400

    Stubs for timestamp/tags

---
+++ cli/src/Models/Augmentations/Tags.hs
@@ -0,0 +1,30 @@
+module Models.Augmentations.Tags where
+
+import Graph.GraphMetadataEditing
+import Graph.SystemNodes
+import Lang.Path.TH
+import Models.Common
+import Models.NID
+import Models.Node
+import MyPrelude
+
+newtype Tags = Tags {tags :: Set Text}
+  deriving (Eq, Show, Ord, Generic)
+
+instance ShowableAugmentation Tags where
+  augmentationProperties (Tags ts)
+    | null ts = singletonMap "tags" Nothing
+    | otherwise = singletonMap "tags" (Just $ intercalate ", " (toList ts))
+
+fetchTags ::
+  (Members '[GraphMetadataReading] r) =>
+  NID ->
+  Sem r Tags
+fetchTags nid = undefined
+
+writeTags ::
+  (Members '[GraphMetadataEditing] r) =>
+  NID ->
+  Tags ->
+  Sem r ()
+writeTags nid tags = undefined
---
+++ cli/src/Models/Augmentations/Timestamps.hs
@@ -0,0 +1,33 @@
+module Models.Augmentations.Timestamps where
+
+import Graph.GraphMetadataEditing
+import Graph.SystemNodes
+import Lang.Path.TH
+import Models.Common
+import Models.NID
+import Models.Node
+import MyPrelude
+
+newtype Timestamps = Timestamps {timestamps :: Set UTCTime}
+  deriving (Eq, Show, Ord, Generic)
+
+instance ShowableAugmentation Timestamps where
+  augmentationProperties (Timestamps ts) = case fromNullable ts of
+    Nothing -> singletonMap "timestamp" Nothing
+    Just ts' ->
+      singletonMap "timestamp" $
+        Just . pack $
+          formatTime defaultTimeLocale "%Y-%m-%d %H:%M" (maximum ts')
+
+fetchTimestamps ::
+  (Members '[GraphMetadataReading] r) =>
+  NID ->
+  Sem r Timestamps
+fetchTimestamps nid = undefined
+
+writeTimestamps ::
+  (Members '[GraphMetadataEditing] r) =>
+  NID ->
+  Timestamps ->
+  Sem r ()
+writeTimestamps nid timestamps = undefined
