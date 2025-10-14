164b33997e9e9799602f7005c6eddacbac6b7bdd
Devin Lehmacher <lehmacdj@gmail.com>
2025-10-13 23:05:35 -0400

    Optimize tags query

--- cli/src/Models/Augmentation/Tags.hs
+++ cli/src/Models/Augmentation/Tags.hs
@@ -18,12 +18,21 @@ instance ShowableAugmentation Tags where
   augmentationProperties (Tags ts) =
     [(Just "tags", ("[" ++) . (++ "]") $ intercalate ", " (toList ts))]
 
+-- | These system nodes can be fairly large and take a long-ish time to parse /
+-- traverse so we explicitly include them from the tag traversal
+excludedLargeSystemNodes :: Path Text
+excludedLargeSystemNodes =
+  ExcludingNIDs $ setFromList [fileHashesNID, importUrlsNID, sequenceIDsNID]
+
 fetchTags ::
   (Members '[GraphMetadataReading] r) =>
   NID ->
   Sem r Tags
 fetchTags nid = do
-  mp <- materializePath nid [path| ~*/~*/%{Absolute tagsNID} |]
+  mp <-
+    materializePath
+      nid
+      [path| ~*/%{excludedLargeSystemNodes}/~*/%{Absolute tagsNID} |]
   let tags = mapSet (.transition) $ concat (finalNonLoopEdges mp.path)
   pure $ Tags tags
 
