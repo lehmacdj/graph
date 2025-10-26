52b74bacc3d5160d716243c15fdfaee0e5cb360d
Devin Lehmacher <lehmacdj@gmail.com>
2025-10-25 23:29:03 -0400

    Preserve annotations in beside operator

--- cli/src/Utils/Prettyprinter.hs
+++ cli/src/Utils/Prettyprinter.hs
@@ -8,11 +8,37 @@ where
 
 import MyPrelude
 import Prettyprinter as X
+import Prettyprinter.Internal qualified as PP
 import Prettyprinter.Render.Terminal as X
 import Utils.Testing
 
 data HorizontalWidth = Fixed Int | Weighted Double
   deriving (Show, Eq)
+
+-- | Reconstruct a Doc from a SimpleDocStream
+-- This allows us to work with laid-out documents while preserving annotations
+streamToDoc :: PP.SimpleDocStream ann -> Doc ann
+streamToDoc = go []
+  where
+    go :: [ann] -> PP.SimpleDocStream ann -> Doc ann
+    go _ PP.SFail = emptyDoc
+    go _ PP.SEmpty = emptyDoc
+    go annStack (PP.SChar c rest) = pretty c <> go annStack rest
+    go annStack (PP.SText _ txt rest) = pretty txt <> go annStack rest
+    go annStack (PP.SLine i rest) = hardline <> pretty (pack (replicate i ' ' :: [Char]) :: Text) <> go annStack rest
+    go annStack (PP.SAnnPush ann rest) = annotate ann (goUntilPop (ann : annStack) rest)
+    go annStack (PP.SAnnPop rest) = go annStack rest
+
+    -- Consume content until we hit the matching SAnnPop
+    goUntilPop :: [ann] -> PP.SimpleDocStream ann -> Doc ann
+    goUntilPop _ PP.SFail = emptyDoc
+    goUntilPop _ PP.SEmpty = emptyDoc
+    goUntilPop annStack (PP.SChar c rest) = pretty c <> goUntilPop annStack rest
+    goUntilPop annStack (PP.SText _ txt rest) = pretty txt <> goUntilPop annStack rest
+    goUntilPop annStack (PP.SLine i rest) = hardline <> pretty (pack (replicate i ' ' :: [Char]) :: Text) <> goUntilPop annStack rest
+    goUntilPop annStack (PP.SAnnPush ann rest) = annotate ann (goUntilPop (ann : annStack) rest)
+    goUntilPop (_:restStack) (PP.SAnnPop rest) = go restStack rest
+    goUntilPop [] (PP.SAnnPop rest) = go [] rest  -- Shouldn't happen, but handle gracefully
 
 -- Layout multiple documents side-by-side with flexible width allocation
 beside :: [(Doc ann, HorizontalWidth)] -> Doc ann
@@ -51,44 +77,85 @@ calculateWidths totalWidth items =
    in map assign items
 
 -- Render documents side-by-side with specified widths (bounded case)
---
--- NOTE: This implementation does not preserve annotations. The documents are
--- rendered to Text and then reconstructed. To preserve annotations, we would
--- need to work with SimpleDocStream directly and implement line-splitting at
--- that level, which is significantly more complex.
+-- This implementation preserves annotations by working with SimpleDocStream
 renderMultiColumn :: [(Int, Doc ann)] -> Doc ann
 renderMultiColumn [] = emptyDoc
 renderMultiColumn items =
-  let -- Render each document at its allocated width
-      rendered = [(w, renderAtWidth w doc) | (w, doc) <- items]
+  let -- Layout each document at its allocated width
+      layouted = [(w, layoutPretty (LayoutOptions (AvailablePerLine w 1.0)) doc) | (w, doc) <- items]
 
-      -- Split into lines
-      lineGroups = [(w, lines txt) | (w, txt) <- rendered]
+      -- Split each laid-out stream into lines (preserving annotations)
+      lineGroups = [(w, splitStreamLines stream) | (w, stream) <- layouted]
 
       -- Find maximum line count
       maxLines = if null lineGroups then 0 else maximumEx ([length ls | (_, ls) <- lineGroups] ++ [0])
 
-      -- Pad each column to maxLines
+      -- Pad each column to maxLines with empty streams
       paddedGroups = [(w, padLines maxLines ls) | (w, ls) <- lineGroups]
 
-      -- Combine lines horizontally
+      -- Combine lines horizontally (preserving annotations)
+      -- Don't pad the last column
       combinedLines =
-        [ concat [leftPad w (fromMaybe "" $ index ls i) | (w, ls) <- paddedGroups]
+        [ mconcat $ zipWith
+            (\isLast (w, ls) -> streamToDoc $ (if isLast then id else padStreamToWidth w) (fromMaybe PP.SEmpty $ index ls i))
+            (if null paddedGroups then [] else replicate (length paddedGroups - 1) False ++ [True])
+            paddedGroups
           | i <- [0 .. maxLines - 1]
         ]
-   in vsep (map pretty combinedLines)
+   in vsep combinedLines
   where
-    renderAtWidth :: Int -> Doc ann -> Text
-    renderAtWidth w doc =
-      renderStrict $ layoutPretty (LayoutOptions (AvailablePerLine w 1.0)) $ unAnnotate doc
+    -- Split a SimpleDocStream into lines at SLine boundaries
+    splitStreamLines :: PP.SimpleDocStream ann -> [PP.SimpleDocStream ann]
+    splitStreamLines = go
+      where
+        go PP.SFail = []
+        go PP.SEmpty = [PP.SEmpty]
+        go (PP.SChar c rest) = consToFirstLine (PP.SChar c PP.SEmpty) (go rest)
+        go (PP.SText len txt rest) = consToFirstLine (PP.SText len txt PP.SEmpty) (go rest)
+        go (PP.SLine _ rest) = PP.SEmpty : go rest  -- New line starts here, discard indentation
+        go (PP.SAnnPush ann rest) =
+          case go rest of
+            [] -> [PP.SAnnPush ann PP.SEmpty]
+            (firstLine : otherLines) -> PP.SAnnPush ann firstLine : otherLines
+        go (PP.SAnnPop rest) = consToFirstLine (PP.SAnnPop PP.SEmpty) (go rest)
 
-    padLines n ls = ls ++ replicate (n - length ls) ""
+        consToFirstLine :: PP.SimpleDocStream ann -> [PP.SimpleDocStream ann] -> [PP.SimpleDocStream ann]
+        consToFirstLine s [] = [s]
+        consToFirstLine s (firstLine : rest) = appendStream s firstLine : rest
 
-    leftPad :: Int -> Text -> Text
-    leftPad w txt =
-      let len = length txt
-          padding = if w > len then pack (replicate (w - len) ' ') else ""
-       in txt <> padding
+    -- Append two SimpleDocStreams
+    appendStream :: PP.SimpleDocStream ann -> PP.SimpleDocStream ann -> PP.SimpleDocStream ann
+    appendStream PP.SFail r = r
+    appendStream PP.SEmpty r = r
+    appendStream (PP.SChar c rest) r = PP.SChar c (appendStream rest r)
+    appendStream (PP.SText len txt rest) r = PP.SText len txt (appendStream rest r)
+    appendStream (PP.SLine i rest) r = PP.SLine i (appendStream rest r)
+    appendStream (PP.SAnnPush ann rest) r = PP.SAnnPush ann (appendStream rest r)
+    appendStream (PP.SAnnPop rest) r = PP.SAnnPop (appendStream rest r)
+
+    padLines :: Int -> [PP.SimpleDocStream ann] -> [PP.SimpleDocStream ann]
+    padLines n ls = ls ++ replicate (n - length ls) PP.SEmpty
+
+    -- Pad a stream to a specific width by measuring and adding spaces
+    padStreamToWidth :: Int -> PP.SimpleDocStream ann -> PP.SimpleDocStream ann
+    padStreamToWidth targetWidth stream =
+      let currentWidth = streamWidth stream
+          paddingNeeded = max 0 (targetWidth - currentWidth)
+       in if paddingNeeded > 0
+            then appendStream stream (PP.SText paddingNeeded (pack (replicate paddingNeeded ' ')) PP.SEmpty)
+            else stream
+
+    -- Calculate the display width of a SimpleDocStream
+    streamWidth :: PP.SimpleDocStream ann -> Int
+    streamWidth = go
+      where
+        go PP.SFail = 0
+        go PP.SEmpty = 0
+        go (PP.SChar _ rest) = 1 + go rest
+        go (PP.SText len _ rest) = len + go rest
+        go (PP.SLine _ rest) = 0 + go rest  -- Lines don't contribute to width
+        go (PP.SAnnPush _ rest) = go rest   -- Annotations don't contribute to width
+        go (PP.SAnnPop rest) = go rest
 
 -- Render unbounded case: measure each document and lay out precisely
 renderMultiColumnUnbounded :: Int -> [(Doc ann, HorizontalWidth)] -> Doc ann
@@ -168,6 +235,15 @@ exampleWithLeftContent =
         (fillSep $ map pretty $ words ("Second column text" :: Text), Weighted 1.0)
       ]
 
+-- Example with annotations (colors) to demonstrate annotation preservation
+exampleWithAnnotations :: Doc AnsiStyle
+exampleWithAnnotations =
+  beside
+    [ (annotate (color Red) $ fillSep $ map pretty $ words ("Red column text" :: Text), Weighted 1.0),
+      (annotate (color Green) $ fillSep $ map pretty $ words ("Green column text" :: Text), Weighted 1.0),
+      (annotate (color Blue) $ fillSep $ map pretty $ words ("Blue column" :: Text), Weighted 1.0)
+    ]
+
 -- Tests
 test_examples :: TestTree
 test_examples =
@@ -196,5 +272,10 @@ test_examples =
           layoutPretty (LayoutOptions (AvailablePerLine 80 1.0)) exampleWithLeftContent,
       goldenTest "example1-60width-reflowing" $
         renderStrict $
-          layoutPretty (LayoutOptions (AvailablePerLine 60 1.0)) example1
+          layoutPretty (LayoutOptions (AvailablePerLine 60 1.0)) example1,
+      -- Test annotation preservation by rendering with ANSI colors
+      goldenTestBinary "example-with-annotations-80width" $
+        encodeUtf8 $
+          renderStrict $
+            layoutPretty (LayoutOptions (AvailablePerLine 80 1.0)) exampleWithAnnotations
     ]
