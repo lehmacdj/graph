71810a192ffbf760007839bab81f9f2e1431b32d
Devin Lehmacher <lehmacdj@gmail.com>
2025-11-02 17:25:59 -0500

    Re-work geometry utilities

--- Nodal/Nodal/Utils/Geometry.swift
+++ Nodal/Nodal/Utils/Geometry.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 extension CGSize {
     static func +(lhs: CGSize, rhs: CGSize) -> CGSize {
@@ -32,7 +33,11 @@ extension CGSize {
         CGSize(width: lhs.width * rhs, height: lhs.height * rhs)
     }
 
-    var vector: CGPoint {
+    var vector: CGVector {
+        CGVector(dx: width, dy: height)
+    }
+
+    var point: CGPoint {
         CGPoint(x: width, y: height)
     }
 
@@ -42,21 +47,74 @@ extension CGSize {
 }
 
 extension CGPoint {
-    static func -(lhs: CGPoint, rhs: CGPoint) -> CGSize {
-        CGSize(width: lhs.x - rhs.x, height: lhs.y - rhs.y)
+    static func +(lhs: CGPoint, rhs: CGVector) -> CGPoint {
+        CGPoint(x: lhs.x + rhs.dx, y: lhs.y + rhs.dy)
     }
 
-    static func -(lhs: CGPoint, rhs: CGSize) -> CGPoint {
-        CGPoint(x: lhs.x - rhs.width, y: lhs.y - rhs.height)
+    static func -(lhs: CGPoint, rhs: CGPoint) -> CGVector {
+        CGVector(dx: lhs.x - rhs.x, dy: lhs.y - rhs.y)
+    }
+
+    static func -(lhs: CGPoint, rhs: CGVector) -> CGPoint {
+        CGPoint(x: lhs.x - rhs.dx, y: lhs.y - rhs.dy)
     }
 
     var size: CGSize {
         CGSize(width: x, height: y)
+    }
+
+    var vector: CGVector {
+        CGVector(dx: x, dy: y)
+    }
+}
+
+extension CGVector {
+    static func *(lhs: CGVector, rhs: CGFloat) -> CGVector {
+        CGVector(dx: lhs.dx * rhs, dy: lhs.dy * rhs)
+    }
+
+    static func +(lhs: CGVector, rhs: CGVector) -> CGVector {
+        CGVector(dx: lhs.dx + rhs.dy, dy: lhs.dy + rhs.dy)
+    }
+
+    static func -(lhs: CGVector, rhs: CGVector) -> CGVector {
+        CGVector(dx: lhs.dx - rhs.dx, dy: lhs.dy - rhs.dy)
+    }
+
+    static func /(lhs: CGVector, rhs: CGFloat) -> CGVector {
+        CGVector(dx: lhs.dx / rhs, dy: lhs.dy - rhs)
+    }
+
+    var point: CGPoint {
+        CGPoint(x: dx, y: dy)
+    }
+
+    var size: CGSize {
+        CGSize(width: dx, height: dy)
     }
 }
 
 extension CGRect {
     var center: CGPoint {
         CGPoint(x: midX, y: midY)
+    }
+
+    static func +(lhs: CGRect, rhs: CGVector) -> CGRect {
+        CGRect(origin: lhs.origin + rhs, size: lhs.size)
+    }
+
+    static func -(lhs: CGRect, rhs: CGVector) -> CGRect {
+        CGRect(origin: lhs.center - rhs, size: lhs.size)
+    }
+}
+
+extension UnitPoint {
+    func `in`(frame: CGRect) -> CGPoint {
+        CGPoint(x: x * frame.width, y: y * frame.height) + frame.origin.vector
+    }
+
+    init(_ point: CGPoint, relativeTo rect: CGRect) {
+        let opoint = point - rect.origin.vector
+        self.init(x: opoint.x / rect.size.width, y: opoint.y / rect.height)
     }
 }
