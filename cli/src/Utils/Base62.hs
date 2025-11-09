d9917d1d91e041bc7feb653b1b765ee98ba7a390
Devin Lehmacher <lehmacdj@gmail.com>
2025-11-09 10:50:10 -0500

    Test change

--- cli/src/Utils/Base62.hs
+++ cli/src/Utils/Base62.hs
@@ -6,7 +6,7 @@ import Data.Char (isAsciiLower, isAsciiUpper, isDigit)
 import MyPrelude
 
 isBase62Char :: Char -> Bool
-isBase62Char c = isDigit c || isAsciiUpper c || isAsciiLower c
+isBase62Char c =   isDigit c   ||   isAsciiUpper c   ||   isAsciiLower c
 
 base62Chars :: Vector Char
-base62Chars = ['0' .. '9'] <> ['A' .. 'Z'] <> ['a' .. 'z']
+base62Chars =    ['0' .. '9']    <>    ['A' .. 'Z']    <>    ['a' .. 'z']
