e90f100055cae5675ba541af3e3618527957b0e8
Devin Lehmacher <lehmacdj@gmail.com>
2025-11-16 12:29:56 -0500

    Factor traversal out of materializeNPath

--- cli/src/Graph/MaterializePath.hs
+++ cli/src/Graph/MaterializePath.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-
 module Graph.MaterializePath where
 
 import Graph.GraphMetadataEditing
@@ -24,8 +22,6 @@ materializePath ::
 materializePath nid path =
   materializeNPath nid (leastConstrainedNormalizedPath (normalizePath path))
 
-{-# HLINT ignore materializeNPath "Functor law" #-}
-
 -- | Traverse a path, fetching node metadata and noting which nodes are missing.
 materializeNPath ::
   forall r.
@@ -36,107 +32,32 @@ materializeNPath ::
   NID ->
   NormalizedPath FullyAnchored ->
   Sem r MaterializedPath
-materializeNPath firstNid normalizedPath = do
-  traverse (traverseDeterministicPath firstNid) (toList normalizedPath.union)
-    & fmap (NormalizedPath . setFromList . concat)
+materializeNPath firstNid normalizedPath =
+  traverseViaTransitions firstNid unifyAnchor traverseDirection normalizedPath
     & cachingReadingInState
     & runState emptyGraph
     & runState mempty
     & fmap \(nonexistentNodes, (graph, path)) -> MaterializedPath {..}
   where
-    traverseDeterministicPath ::
+    unifyAnchor :: Bool -> NID -> FullyAnchored -> Maybe NID
+    unifyAnchor isRoot nid = \case
+      FSpecific fnid
+        | not isRoot && nid /= fnid -> Nothing
+        | otherwise -> pure fnid
+      FJoinPoint {..}
+        | nid `notMember` excluding -> pure nid
+        | otherwise -> Nothing
+
+    traverseDirection ::
       ( Members [GraphMetadataReading, State (Graph Text IsThin), State (Set NID)] q,
         HasCallStack
       ) =>
       NID ->
-      DeterministicPath FullyAnchored ->
-      Sem q [DeterministicPath NID]
-    traverseDeterministicPath nid = \case
-      Pointlike p -> map Pointlike <$> traversePointlike True nid p
-      Rooted r -> map Rooted <$> traverseRooted nid r
-
-    traversePointlike ::
-      ( Members [GraphMetadataReading, State (Graph Text IsThin), State (Set NID)] q,
-        HasCallStack
-      ) =>
-      Bool ->
-      NID ->
-      PointlikeDeterministicPath FullyAnchored ->
-      Sem q [PointlikeDeterministicPath NID]
-    traversePointlike isRoot nid PointlikeDeterministicPath {..} = withEarlyReturn do
-      nid' <- case anchor of
-        FSpecific fnid
-          | not isRoot && nid /= fnid -> returnEarly []
-          | otherwise -> pure fnid
-        FJoinPoint {..}
-          | nid `notMember` excluding -> pure nid
-          | otherwise -> returnEarly []
-      loops' <- traverseBranches nid' loops
-      pure $
-        loops'
-          & filter ((== nid') . snd)
-          & map (PointlikeDeterministicPath nid' . fst)
-
-    ensureSameAnchors ::
-      (HasCallStack) => NonNull [(a, NID)] -> Maybe (NonNull [a], NID)
-    ensureSameAnchors xs =
-      let example = snd (head xs)
-          as = mapNonNull fst xs
-       in justIfTrue
-            (all ((== example) . snd) $ toNullable xs)
-            (as, example)
-
-    traverseRooted ::
-      ( Members [GraphMetadataReading, State (Graph Text IsThin), State (Set NID)] q,
-        HasCallStack
-      ) =>
-      NID ->
-      RootedDeterministicPath FullyAnchored ->
-      Sem q [RootedDeterministicPath NID]
-    traverseRooted nid RootedDeterministicPath {..} = do
-      rootBranches' ::
-        [ [ ( DeterministicPath NID,
-              OSet (DPBranch FullyAnchored)
-            )
-          ]
-        ] <-
-        rootBranches
-          & mapToList
-          & (traverse . _1) (traverseDeterministicPath nid)
-          <&> map (\(rs, bs) -> (,bs) <$> rs)
-          <&> choices
-      rootBranches'' ::
-        [(OMap (DeterministicPath NID) (OSet (DPBranch NID)), NID)] <-
-        rootBranches'
-          & (traverse . traverse)
-            (\(dp, bs) -> (dp,) <$> traverseBranches dp.target.anchor bs)
-          <&> map (map \(r, bas) -> (\(b, a) -> ((r, b), a)) <$> bas)
-          <&> concatMap choices
-          <&> mapMaybe (ensureSameAnchors . impureNonNull)
-          <&> ordNub
-          <&> over (mapped . _1) (mapFromList . toNullable)
-      rootBranches''
-        & (traverse . _2) (\x -> traversePointlike False x target)
-        <&> concatMap (\(rbs, ts) -> (rbs,) <$> ts)
-        <&> map (uncurry smartBuildRootedDeterministicPath)
-
-    traverseBranches ::
-      ( Members [GraphMetadataReading, State (Graph Text IsThin), State (Set NID)] q,
-        HasCallStack
-      ) =>
-      NID ->
-      OSet (DPBranch FullyAnchored) ->
-      Sem q [(OSet (DPBranch NID), NID)]
-    traverseBranches nid branches = withEarlyReturn do
-      -- in the case where the OSet is empty (only when loops),
-      -- we return the same anchor as the input
-      when (null branches) $ returnEarly [(mempty, nid)]
-      branches' <- traverse (traverseBranch nid) (toList branches)
-      pure $
-        branches'
-          & choices
-          & mapMaybe (ensureSameAnchors . impureNonNull)
-          & over (mapped . _1) (setFromList . toNullable)
+      DPDirection ->
+      Sem q [(DPDirection, NID)]
+    traverseDirection nid = \case
+      DPIncoming' t -> traverseTransition nid (.incoming) DPIncoming' t
+      DPOutgoing' t -> traverseTransition nid (.outgoing) DPOutgoing' t
 
     traverseTransition ::
       ( Members [GraphMetadataReading, State (Graph Text IsThin), State (Set NID)] q,
@@ -144,9 +65,9 @@ materializeNPath firstNid normalizedPath = do
       ) =>
       NID ->
       (Node Text () -> Set (Connect Text)) ->
-      (DPTransition -> DPBranch NID) ->
+      (DPTransition -> DPDirection) ->
       DPTransition ->
-      Sem q [(DPBranch NID, NID)]
+      Sem q [(DPDirection, NID)]
     traverseTransition nid getConnections mkBranch = \case
       DPLiteral t -> withEarlyReturn do
         n <- getNodeMetadata nid `onNothingM` returnEarly []
@@ -166,23 +87,3 @@ materializeNPath firstNid normalizedPath = do
           getConnections n
             & toListOf (folded . filteredBy (#transition . textRegexing r))
             & map (\Connect {..} -> (mkBranch (DPLiteral transition), node))
-
-    traverseBranch ::
-      ( Members [GraphMetadataReading, State (Graph Text IsThin), State (Set NID)] q,
-        HasCallStack
-      ) =>
-      NID ->
-      DPBranch FullyAnchored ->
-      Sem q [(DPBranch NID, NID)]
-    traverseBranch nid = \case
-      DPOutgoing transition -> traverseTransition nid (.outgoing) DPOutgoing transition
-      DPIncoming transition -> traverseTransition nid (.incoming) DPIncoming transition
-      DPSequence bs1 midpoint bs2 -> do
-        bs1's <- traverseBranches nid bs1
-        uptoMidpoints :: [(OSet (DPBranch NID), PointlikeDeterministicPath NID)] <-
-          bs1's
-            & (traverse . _2) (\x -> traversePointlike False x midpoint)
-            <&> concatMap (\(bs1', ms) -> (bs1',) <$> ms)
-        uptoMidpoints
-          & (traverse . _2) (\p -> (p,) <$> traverseBranches p.anchor bs2)
-          <&> concatMap (\(bs1', (p, bs2'ts)) -> first (DPSequence bs1' p) <$> bs2'ts)
--- cli/src/Models/NormalizedPath.hs
+++ cli/src/Models/NormalizedPath.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 module Models.NormalizedPath where
 
@@ -573,3 +575,121 @@ traverseAnchors f (NormalizedPath paths) =
           <$> traverse convertBranch (toList as1)
           <*> convertPointlike midpoint
           <*> (setFromList <$> traverse convertBranch (toList as2))
+
+{-# HLINT ignore traverseViaTransitions "Functor law" #-}
+
+traverseViaTransitions ::
+  forall f a b.
+  (Monad f, Ord a, Ord b) =>
+  b ->
+  (Bool -> b -> a -> Maybe b) ->
+  (b -> DPDirection -> f [(DPDirection, b)]) ->
+  NormalizedPath a ->
+  f (NormalizedPath b)
+traverseViaTransitions initial fanchor ftransition normalizedPath =
+  traverse (traverseDeterministicPath initial) (toList normalizedPath.union)
+    <&> NormalizedPath . setFromList . concat
+  where
+    traverseDeterministicPath ::
+      ( Applicative f,
+        HasCallStack
+      ) =>
+      b ->
+      DeterministicPath a ->
+      f [DeterministicPath b]
+    traverseDeterministicPath nid = \case
+      Pointlike p -> map Pointlike <$> traversePointlike True nid p
+      Rooted r -> map Rooted <$> traverseRooted nid r
+
+    traversePointlike ::
+      (HasCallStack) =>
+      Bool ->
+      b ->
+      PointlikeDeterministicPath a ->
+      f [PointlikeDeterministicPath b]
+    traversePointlike isRoot nid PointlikeDeterministicPath {..} = do
+      case fanchor isRoot nid anchor of
+        Nothing -> pure []
+        Just nid' -> do
+          loops' <- traverseBranches nid' loops
+          pure $
+            loops'
+              & filter ((== nid') . snd)
+              & map (PointlikeDeterministicPath nid' . fst)
+
+    ensureSameAnchors ::
+      forall x. (HasCallStack) => NonNull [(x, b)] -> Maybe (NonNull [x], b)
+    ensureSameAnchors xs =
+      let example = snd (head xs)
+          as = mapNonNull fst xs
+       in justIfTrue
+            (all ((== example) . snd) $ toNullable xs)
+            (as, example)
+
+    traverseRooted ::
+      (HasCallStack) =>
+      b ->
+      RootedDeterministicPath a ->
+      f [RootedDeterministicPath b]
+    traverseRooted nid RootedDeterministicPath {..} = do
+      rootBranches' ::
+        [ [ ( DeterministicPath b,
+              OSet (DPBranch a)
+            )
+          ]
+        ] <-
+        rootBranches
+          & mapToList
+          & (traverse . _1) (traverseDeterministicPath nid)
+          <&> map (\(rs, bs) -> (,bs) <$> rs)
+          <&> choices
+      rootBranches'' ::
+        [(OMap (DeterministicPath b) (OSet (DPBranch b)), b)] <-
+        rootBranches'
+          & (traverse . traverse)
+            (\(dp, bs) -> (dp,) <$> traverseBranches dp.target.anchor bs)
+          <&> map (map \(r, bas) -> (\(b, a) -> ((r, b), a)) <$> bas)
+          <&> concatMap choices
+          <&> mapMaybe (ensureSameAnchors . impureNonNull)
+          <&> ordNub
+          <&> over (mapped . _1) (mapFromList . toNullable)
+      rootBranches''
+        & (traverse . _2) (\x -> traversePointlike False x target)
+        <&> concatMap (\(rbs, ts) -> (rbs,) <$> ts)
+        <&> map (uncurry smartBuildRootedDeterministicPath)
+
+    traverseBranches ::
+      (HasCallStack) =>
+      b ->
+      OSet (DPBranch a) ->
+      f [(OSet (DPBranch b), b)]
+    traverseBranches nid branches
+      | -- in the case where the OSet is empty (only when loops),
+        -- we return the same anchor as the input
+        null branches =
+          pure [(mempty, nid)]
+      | otherwise = do
+          branches' <- traverse (traverseBranch nid) (toList branches)
+          pure $
+            branches'
+              & choices
+              & mapMaybe (ensureSameAnchors . impureNonNull)
+              & over (mapped . _1) (setFromList . toNullable)
+
+    traverseBranch ::
+      (HasCallStack) =>
+      b ->
+      DPBranch a ->
+      f [(DPBranch b, b)]
+    traverseBranch nid = \case
+      DPSingle direction ->
+        over (mapped . _1) DPSingle <$> ftransition nid direction
+      DPSequence bs1 midpoint bs2 -> do
+        bs1's <- traverseBranches nid bs1
+        uptoMidpoints :: [(OSet (DPBranch b), PointlikeDeterministicPath b)] <-
+          bs1's
+            & (traverse . _2) (\x -> traversePointlike False x midpoint)
+            <&> concatMap (\(bs1', ms) -> (bs1',) <$> ms)
+        uptoMidpoints
+          & (traverse . _2) (\p -> (p,) <$> traverseBranches p.anchor bs2)
+          <&> concatMap (\(bs1', (p, bs2'ts)) -> first (DPSequence bs1' p) <$> bs2'ts)
